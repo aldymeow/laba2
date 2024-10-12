5ecb3305d6d81458bc80725dcca8bcf72a1f55d2
tima <tima.shirinkin@gmail.com>
2024-10-13 00:03:06 +0500

    example3

---
+++ examples/example3.py
@@ -0,0 +1,10 @@
+def example(color):
+    if color == "green":
+        return "this is trava"
+    elif color == "blue":
+        return "this is voda"
+    else:
+        return "i dont know"
+    
+what_is_it = example('blue')
+print (what_is_it)
