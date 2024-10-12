36c6bfc0f8d878c9ecde951809726b8feeac2ffb
tima <tima.shirinkin@gmail.com>
2024-10-13 02:07:17 +0500

    homework3 - new proverka

--- homework3/homework3.py
+++ homework3/homework3.py
@@ -6,7 +6,9 @@ def input_age(prompt):
             print("Ошибка! Введите число.")
 
 def age():
-    if d1 >= d and m1 >= m:
+    if m1 >= m:
+        return print ('На сегодняшний день Вам ', y1-y, 'лет')
+    elif m1 == m and d1 >= d:
         return print ('На сегодняшний день Вам ', y1-y, 'лет')
     else:
         return print('На сегодняшний день Вам ', y1-y-1, 'лет')
@@ -19,8 +21,8 @@ d1 = input_age("Введите сегодняшний день (1-31): ")
 m1 = input_age("Введите какой сейчас месяц (1-12): ")
 y1 = input_age("Введите какой сейчас год: ")
 
-if (1 <= d <= 31) and (1 <= m <= 12) and (1 <= y <= 2024) and (1 <= d1 <= 31) and (1 <= m1 <= 12) and (1 <= y1 <= 2024):
-    user_age = age()
-    print (user_age)
+if (1 <= d <= 31) and (1 <= m <= 12) and (1 <= y <= 2024) and (1 <= d1 <= 31) \
+and (1 <= m1 <= 12) and (1 <= y1 <= 2024) and (y <= y1):
+    age()
 else:
     print("Необходимо ввести реальные даты")
